402a1335fc44a4ab019706c4be66ca390cab1b0a
Nickita Rusanov <nipe322@gmail.com>
2024-04-06 18:26:50 +0300

    Add validation for user req

--- back/service.py
+++ back/service.py
@@ -1,14 +1,16 @@
 from fastapi import APIRouter
 
-import schemas
+import back.schemas as schemas
+from core.validator import correct_req
 
 
 router = APIRouter()
 
 @router.get('/ste', response_model=schemas.STE)
 def get_ste(user_req: str):
+    valid_name = correct_req(user_req)
     ste = schemas.STE(
-        fullname="Test",
+        fullname=valid_name,
         chars=[
             'size',
             'type'
