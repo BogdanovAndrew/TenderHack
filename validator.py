86824729872aa1565a68055936af382b2036bcae
Nickita Rusanov <nipe322@gmail.com>
2024-04-06 13:37:59 +0300

    Fix validator

--- validator.py
+++ validator.py
@@ -2,10 +2,16 @@ import requests
 
 
 def correct_req(req: str) -> str:
-    resp = requests.get(f"https://speller.yandex.net/services/spellservice.json/checkText?text={req}")
-    correct = " ".join(word['s'][0] for word in resp.json())
-    
-    return correct
+    correct_words = []
+    for word in req.split():
+        resp = requests.get(f"https://speller.yandex.net/services/spellservice.json/checkText?text={word}").json()
+        if resp:
+            correct_words.append(resp[0]['s'][0])
+        else:
+            correct_words.append(word)
+    return " ".join(correct_words)
 
 
-print(correct_req("Валек молярный срукояткой"))
+
+
+print(correct_req("Ноутбука aple"))
